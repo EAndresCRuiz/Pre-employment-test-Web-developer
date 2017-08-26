d88b70cc89f6c63dd7ebcb2bdcf2c9fb4cf33d6d
Andres <eder.ac.ruiz@gmail.com>
2017-08-25 23:19:09 -0500

    Correction on task uglify

--- Gruntfile.js
+++ Gruntfile.js
@@ -33,7 +33,7 @@ module.exports = function(grunt) {
 				},
 				dist: {
 					files: {
-						'dist/<%= pkg.name %>.min.js': ['<%= concat.dist.dest %>']
+						'public/scrip.min.js': ['<%= concat.dist.dest %>']
 					}
 				}
 		},
