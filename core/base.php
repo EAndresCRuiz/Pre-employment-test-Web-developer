bbd191fe51f6a72c96d1ae8c9a67df340d5145c1
Andres <eder.ac.ruiz@gmail.com>
2017-08-25 22:07:49 -0500

    Initial commit, handle error database connection

--- core/base.php
+++ core/base.php
@@ -1,13 +1,11 @@
 <?php 
-
 class base
 {
     public function __construct()
     {
         // grab the DB config settings from a ini file
         // http://php.net/manual/en/function.parse-ini-file.php
-        $settings = parse_ini_file('config.php', TRUE);
-
+        $settings = parse_ini_file('config.sample.php', TRUE);
         // establish connection to DB
         $this->db = new mysqli(
             $settings['DB']['host'],
@@ -15,26 +13,35 @@ class base
             $settings['DB']['pass'],
             $settings['DB']['database']
         );
-
+ 
         // should you handle connection errors?
-        // if($this->db->connect_error) { ... }
+        if($this->db->connect_error) { 
+
+            die("Can't connect to database, check the connection configuration");
+
+        }
     }
-
-
+ 
     protected function result_array( $result )
     {
+ 
+        $result_array = array();
+ 
         while($record = $result->fetch_object())
         {
+ 
             $result_array[] = $record;
+ 
         }
-
+ 
         return $result_array;
+ 
     }
-
-
+ 
     public function __destruct()
     {
+ 
         $this->db->close();
+ 
     }
-
 }
